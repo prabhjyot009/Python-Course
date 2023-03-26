22af679bc44cfa5781044dabbef4ab720db0382c
prabhjyot009 <prabh156dps@gmail.com>
2023-03-26 16:20:32 +0530

    commit

--- Python/My Course/Course/Programs/Practice.py
+++ Python/My Course/Course/Programs/Practice.py
@@ -1,10 +1,5 @@
 #wap to print 1 to n natural number
 
 n=int(input("Enter the number upto which you want to print:"))
-i=1
-sum=0
-while i<=n:
-    sum=sum+i
-    print(i)
-    i=i+1
-print("Sum is",sum)
+while(n>=2):
+    sum
