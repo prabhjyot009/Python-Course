1451f0bde49db43428f346f0eea06c335933de19
prabhjyot009 <prabh156dps@gmail.com>
2023-03-26 23:07:41 +0530

    commit

--- Python/My Course/Course/Programs/Practice.py
+++ Python/My Course/Course/Programs/Practice.py
@@ -34,9 +34,65 @@
 #     print("Not Armstrong")
     
 #product of digits:
-n = int(input("Enter Number"))
-pro = 1
-while n > 0:
-    pro = pro * (n % 10)
-    n = n // 10
-print(pro)
+# n = int(input("Enter Number"))
+# pro = 1
+# while n > 0:
+#     pro = pro * (n % 10)
+#     n = n // 10
+# print(pro)
+
+#sum of even digits and product of odd digits
+# n = int(input("Enter number"))
+# sum = 0
+# pro = 1
+# while n > 0:
+#     if n % 2 == 0:
+#         sum = sum + n % 10
+#         n = n // 10
+#     else:
+#         pro = pro * (n % 10)
+#         n = n // 10
+# print("Sum of even numbers:",sum)
+# print("Product of Odd numbers:",pro)
+
+#Reverse a number
+# n=int(input("Enter"))
+# rev=0
+# while(n>0):
+#     rev=(rev*10)+n%10
+#     n=n//10
+# print("reverse is:",rev)
+
+#Palindrome:
+# n=int(input("Enter"))
+# rev=0
+# temp=n
+# while(n>0):
+#     rev=(rev*10)+n%10
+#     n=n//10
+# if rev==temp:
+#     print(rev,"is Palindrome")
+# else:
+#     print(rev,"is Not Palindrome")
+
+#prime or not
+# n=int(input("Enter"))
+# i=1
+# count=0
+# while(i<=n):
+#     if(n%i==0):
+#         count+=1
+#     i+=1
+# if(count==2):
+#     print("Prime")
+# else:
+#     print("Composite")
+    
+#Factorial
+# n=int(input("Enter: "))
+# fact=1
+# while(n>0):
+#     fact=fact*n
+#     n-=n-1
+# print(fact)
+
