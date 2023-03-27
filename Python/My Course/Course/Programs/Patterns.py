466478fccd450d3fcabca9ea37ea26e6f1d91e03
prabhjyot009 <prabh156dps@gmail.com>
2023-03-27 23:14:24 +0530

    commit

--- Python/My Course/Course/Programs/Patterns.py
+++ Python/My Course/Course/Programs/Patterns.py
@@ -70,20 +70,49 @@
 #   ****
 #  ******
 # ********
+# i=1
+# k=1
+# while i<=5:
+#     b=1
+#     while b<=5-i:
+#         print(" ",end='')
+#         b=b+1
+#     j=1
+#     while j<=k:
+#         print("*",end='')
+#         j=j+1
+#     k=k+2
+#     print()
+#     i=i+1
+    
+#Pyramid numbers:
+# i=1
+# k=1
+# while i<=5:
+#     b=1
+#     while b<=5-i:
+#         print(" ",end='')
+#         b=b+1
+#     j=1
+#     while j<=k:
+#         print(k,end='')
+#         j=j+1
+#     k=k+2
+#     print()
+#     i=i+1
+
+#Reverse Pyramid:
+n=int(input("Enter number of rows:"))
 i=1
-k=1
-while i<=5:
+while n>0:
     b=1
-    while b<=5-i:
+    while b<i:
         print(" ",end='')
         b=b+1
     j=1
-    while j<=k:
+    while j<=(n*2)-1:
         print("*",end='')
         j=j+1
-    k=k+2
     print()
+    n=n-1
     i=i+1
-    
-#Pyramid numbers:
-
