49351b91eb3206273fc2910f8e54757bb07558a7
prabhjyot009 <prabh156dps@gmail.com>
2023-03-27 22:25:06 +0530

    commit

---
+++ Python/My Course/Course/Programs/Patterns.py
@@ -0,0 +1,89 @@
+#print patterns:
+# *
+# **
+# ***
+# ****
+# *****
+
+# i=1
+# while(i<=5):
+#     j=1
+#     while(j<=i):
+#         print("*",end='')
+#         j=j+1
+#     print()
+#     i=i+1
+    
+# 1
+# 22
+# 333
+# 4444
+# 55555
+
+# i=1
+# while i<=5:
+#     j=1
+#     while(j<=i):
+#         print(j,end='')
+#         j=j+1
+#     print()
+#     i=i+1
+    
+#Reverse order
+#     *
+#    **
+#   ***
+#  ****
+# *****
+
+# i=1
+# while i<=5:
+#     b=1
+#     while b<=5-i:
+#         print(" ",end='')
+#         b=b+1
+#     j=1
+#     while j<=i:
+#         print("*",end='')
+#         j=j+1
+#     print()
+#     i=i+1
+    
+#reverse in numbers:
+
+# i=1
+# while i<=5:
+#     b=1
+#     while b<=5-i:
+#         print(" ",end='')
+#         b=b+1
+#     j=1
+#     while j<=i:
+#         print(i,end='')
+#         j=j+1
+#     print()
+#     i=i+1
+    
+#Pyramid
+#     *
+#    ***
+#   ****
+#  ******
+# ********
+i=1
+k=1
+while i<=5:
+    b=1
+    while b<=5-i:
+        print(" ",end='')
+        b=b+1
+    j=1
+    while j<=k:
+        print("*",end='')
+        j=j+1
+    k=k+2
+    print()
+    i=i+1
+    
+#Pyramid numbers:
+
