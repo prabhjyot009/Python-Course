5b3d6dd4f4d6109dbaee11f450cd32cc1592e844
prabhjyot009 <prabh156dps@gmail.com>
2023-04-17 08:55:35 +0530

    commit

--- Python/My Course/Course/Programs/List.py
+++ Python/My Course/Course/Programs/List.py
@@ -1,7 +1,21 @@
 a=["ram",1,"shyam",12.5]
-print(a) 
+print(a)
 print(a[0][-1::-1]) #revrse of 1st element
 print(a[-1])
 a[1]=100
 print(a[1])
 
+
+
+# l=[]
+# # while(a<100 and a%5==0):
+# #         l.append(a)
+# #         a=int(input("Enter again:"))
+# a=int(input("Enter"))
+# for i in range(0,a):
+#     a=int(input("Enter next:"))
+#     if(a%5==0 and a!=100):
+#         l.append(a);
+#     else:
+#         break;
+# print(l)
