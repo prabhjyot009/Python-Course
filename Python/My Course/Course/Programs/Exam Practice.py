fc3aefbf5149ad44eb2b76b8b91b944c3f83a402
prabhjyot009 <prabh156dps@gmail.com>
2023-04-20 00:28:37 +0530

    commit

--- Python/My Course/Course/Programs/Exam Practice.py
+++ Python/My Course/Course/Programs/Exam Practice.py
@@ -106,10 +106,181 @@
 # sum=first_digit*last_digit
 # print(sum)
 
-num = input("Enter a number: ")
-middle = num[1:-1]  # slice the middle digits
-product = 1
-for digit in middle:
-    product *= int(digit)
-print("Product of middle digits:", product)
+# num = input("Enter a number: ")
+# middle = num[1:-1]  # slice the middle digits
+# product = 1
+# for digit in middle:
+#     product *= int(digit)
+# print("Product of middle digits:", product)
 
+
+# num = int(input("Enter a number: "))
+# sum=0
+# while num>0:
+#     sum=sum+(num%10)*(num%10)
+#     num=num//10
+# print(sum)
+
+
+# num = int(input("Enter a number: "))  # taking input from user
+
+# # converting the number to a string for easier manipulation
+# num_str = str(num)
+
+# # interchanging the digits
+# new_num_str = num_str[-1] + num_str[1:-1] + num_str[0]
+
+# # converting the string back to an integer
+# new_num = int(new_num_str)
+
+# print("The number with digits interchanged is:", new_num)
+
+# n=int(input("Enter no."))
+# sum=0
+# for i in range(0,n):
+#     num=int(input("Enter values:"))
+#     sum=sum+num
+# print(sum)
+
+# for i in range(256):
+#     ch = chr(i)
+#     print(i, " ", ch)
+
+# text = input("Enter a String: ")
+# for ch in text:
+#     asc = ord(ch)
+#     print(ch, "\t", asc)
+
+# n = int(input("Enter"))
+
+# fib = list()
+# b = 1
+# c = 0
+# i = 1
+# while i <= n:
+#     if i == 1:
+#         c = 0
+#     elif i == 2 or i == 3:
+#         c = 1
+#     else:
+#         a = b
+#         b = c
+#         c = a+b
+#     fib.append(c)
+#     i = i+1
+
+# if len(fib) == 0:
+#     print("\nOk!")
+# else:
+#     print("\nFirst", n, "Terms of Fibonacci Series are:")
+#     for i in fib:
+#         print(i, end=" ")
+        
+# n = int(input("Enter a number: "))
+# fib_series = [0, 1] # Initialize the list with the first two elements of the series
+# while fib_series[-1] < n:
+#     # While the last element in the list is less than n, add the sum of the last two elements to the list
+#     next_fib = fib_series[-1] + fib_series[-2]
+#     if next_fib < n:
+#         fib_series.append(next_fib)
+#     else:
+#         break
+# fib_list = []
+# for i in fib_series:
+#     if i < n:
+#         fib_list.append(i)
+# print(fib_list)
+
+# n = int(input("Enter the number of elements to be inserted: "))
+# nums = []
+
+# for i in range(n):
+#     num = float(input(f"Enter element {i+1}: "))
+#     nums.append(num)
+
+# avg = sum(nums) / n
+
+# print("Average of elements in the list:", avg)
+
+# n=[]
+# size=int(input("Enter size:"))
+# for i in range(size):
+#     val=int(input("Input values"))
+#     n.append(val)
+# print(n)
+# even=0
+# odd=0
+# for i in range(size):
+#     if n[i]%2==0:
+#         even=even+1
+#     else:
+#         odd=odd+1
+# print(even)
+# print(odd)
+        
+# n=[]
+# size=int(input("Enter size:"))
+# for i in range(size):
+#     val=int(input("Input values"))
+#     n.append(val)
+# print(n)
+# maxno=0
+# minno=0
+# sum=0
+# for i in range(size):
+#     sum=sum+n[i]
+#     if n[i]%2==0:
+#         maxno=max(n)
+#     else:
+#         minno=min(n)
+# print(maxno)
+# print(minno)
+# print(sum)
+
+# n=[]
+# size=int(input("Enter size:"))
+# for i in range(size):
+#     val=int(input("Input values"))
+#     n.append(val)
+# print(n)
+# sum=0
+# for i in range(size):
+#     sum=sum+n[i]
+# print(sum)
+
+# n=[]
+# size=int(input("Enter size:"))
+# for i in range(size):
+#     val=int(input("Input values"))
+#     n.append(val)
+# print(n)
+# for i in range(size):
+#     if i%2==0:
+#         print(n[i])
+
+# n=[]
+# size=int(input("Enter size:"))
+# for i in range(size):
+#     val=int(input("Input values"))
+#     n.append(val)
+# print(n)
+# list2=list(set(n))
+# for i in range(size):
+#     list2.sort()
+# print(list2[1])
+
+# n=[]
+# size=int(input("Enter size:"))
+# for i in range(size):
+#     val=int(input("Input values"))
+#     n.append(val)
+# print(n)
+# ele=int(input("Enter element for insertion:"))
+# n.append(ele)
+# print(n)
+
+n=[]
+size=int(input("Enter size:"))
+for i in range(size):
+    val=int(input("Input values"))
+    n.append(val)
