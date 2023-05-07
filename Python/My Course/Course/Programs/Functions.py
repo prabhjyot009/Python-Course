1a8a6146547b1069b9c33c5f805c42c51e7bc0d5
prabhjyot009 <prabh156dps@gmail.com>
2023-05-07 15:12:01 +0530

    Python

---
+++ Python/My Course/Course/Programs/Functions.py
@@ -0,0 +1,33 @@
+# def oddeven(a):
+#     if a%2==0:
+#         print(a,"even");
+#     else:
+#         print(a,"odd");
+#     return a
+# a=int(input("Enter number:"))
+# x=oddeven(a)
+
+
+# def reverse(i):
+#     rev=0;
+#     while i>0:
+#         rev=(rev*10)+i%10
+#         i=i//10
+#     print(rev)
+#     return rev
+# i=int(input("Enter number:"))
+# x=reverse(i)
+
+def listadd(a):
+    sum=0
+    for i in a:
+        sum=sum+i;
+    print(sum)
+# l=[]
+# size=int(input("Enter size:"))
+# for i in range(size):
+#     val=int(input("Enter values:"))
+#     l.append(val)
+# print(l)
+a=int(input("Enter elemnt to add:"))
+listadd(a)
--- Python/My Course/Course/Programs/Practice.py
+++ Python/My Course/Course/Programs/Practice.py
@@ -358,4 +358,3 @@ for i in range(1,minnum+1):
     if(num1%i==0 and num2%i==0 and num3%i==0):
         hcf=i
 print(hcf)
-
