4d6e8b195c972dd621d572befcf24e70f8a3df81
prabhjyot009 <prabh156dps@gmail.com>
2023-05-16 08:29:05 +0530

    Python

---
+++ Python/My Course/Course/Programs/NUMpy.py
@@ -0,0 +1,12 @@
+import numpy as np
+array =np.array([[1,2,3],[4,5,6]])
+
+print("Array is of type:",type(array))
+print("Number of dimensions:",array.ndim)
+print("Shape of array:",array.shape)
+print("Size of array:",array.size)
+print("DataType value of an array",array.dtype)
+
+arr=np.array([[1,2,3],[4,5,6]],dtype='float')
+print(arr)
+
