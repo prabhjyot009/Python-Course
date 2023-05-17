ae1c4df2e9873c2901d3232427771236bf0f24c5
prabhjyot009 <prabh156dps@gmail.com>
2023-05-17 21:24:57 +0530

    Commit

--- Python/My Course/Course/Programs/Recursion.py
+++ Python/My Course/Course/Programs/Recursion.py
@@ -18,18 +18,31 @@
 # evenn=even(n)
 # print(evenn)
 
-def xyz(S,n):
-    if n==0:
-        print(S[0])
-    else:
-        print(S[n],end='')
-        xyz (S,n-1)
-S=input("Enter String:")
-xyz(S,len(S)-1)
+# def xyz(S,n):
+#     if n==0:
+#         print(S[0])
+#     else:
+#         print(S[n],end='')
+#         xyz (S,n-1)
+# S=input("Enter String:")
+# xyz(S,len(S)-1)
 
 #factorial of a non-negative number:
+# def fact(n):
+#     if n==1:
+#         return 1
+#     else:
+#         return fact(n-1)*n
+# n=int(input("Enter number"))
+# factt=fact(n)
+# print(factt)
+
+#factorial:
 def fact(n):
-    if n==1:
+    if n==1 and n==2:
         return 1
     else:
-        return()
+        return (fact(n-1)+fact(n-2))
+n=int(input("Enter number"))
+factt=fact(n)
+print(factt)
