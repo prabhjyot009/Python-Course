5e4e2654a8954abf4d041f1d872f202ba04af85f
prabhjyot009 <prabh156dps@gmail.com>
2023-05-23 08:40:49 +0530

    py

--- Python/My Course/Course/Programs/NUMpy.py
+++ Python/My Course/Course/Programs/NUMpy.py
@@ -1,48 +1,55 @@
 import numpy as np
-array =np.array([[1,2,3],[4,5,6]])
+# array =np.array([[1,2,3],[4,5,6]])
 
-print("Array is of type:",type(array))
-print("Number of dimensions:",array.ndim)
-print("Shape of array:",array.shape)
-print("Size of array:",array.size)
-print("DataType value of an array",array.dtype)
+# print("Array is of type:",type(array))
+# print("Number of dimensions:",array.ndim)
+# print("Shape of array:",array.shape)
+# print("Size of array:",array.size)
+# print("DataType value of an array",array.dtype)
 
-arr=np.array([[1,2,3],[4,5,6]],dtype='float')
-print(arr)
+# arr=np.array([[1,2,3],[4,5,6]],dtype='float')
+# print(arr)
 
-b=np.array((1,2,3))
-print(b)
+# b=np.array((1,2,3))
+# print(b)
 
-c=np.zeros((3,4))
-print(c)
+# c=np.zeros((3,4))
+# print(c)
 
-f=np.arange(0,30,5)
-print(f)
+# f=np.arange(0,30,5)
+# print(f)
 
-g=np.linspace(0,5,10)
-print(g)
+# g=np.linspace(0,5,10)
+# print(g)
 
-#add two arrays
-x=np.array([1,2,3])
-y=np.array([4,5,6])
-z=x+y
-print(z)
+# #add two arrays
+# x=np.array([1,2,3])
+# y=np.array([4,5,6])
+# z=x+y
+# print(z)
 
-#subtract two arrays
-x=np.array([1,2,3])
-y=np.array([4,5,6])
-z=x-y
-print(z)
+# #subtract two arrays
+# x=np.array([1,2,3])
+# y=np.array([4,5,6])
+# z=x-y
+# print(z)
 
-#multiply two arrays
-x=np.array([1,2,3])
-y=np.array([4,5,6])
-z=x*y
-print(z)
+# #multiply two arrays
+# x=np.array([1,2,3])
+# y=np.array([4,5,6])
+# z=x*y
+# print(z)
 
-#multiply matrix
-x=np.array([[1,2,3],[4,5,6]])
-y=np.array([[1,2,3],[4,5,6]])
-z=x*y
-print(z)
+# #multiply matrix
+# x=np.array([[1,2,3],[4,5,6]])
+# y=np.array([[1,2,3],[4,5,6]])
+# z=x*y
+# print(z)
 
+# x=np.array([[1,2,3],[4,5,6]])
+
+# print(x.max())
+# print("Row wise max ele:",x.max(axis=0))
+# print("Col wise max ele:",x.max(axis=0))
+# print("Row wise min ele:",x.min(axis=1))
+# print("Col wise min ele:",x.min(axis=1))
