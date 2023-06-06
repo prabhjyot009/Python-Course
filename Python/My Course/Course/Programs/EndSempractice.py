1379fc29728d3f19e67103b1e95be10c28998bbf
prabhjyot009 <prabh156dps@gmail.com>
2023-06-06 23:40:04 +0530

    py

---
+++ Python/My Course/Course/Programs/EndSempractice.py
@@ -0,0 +1,15 @@
+    size=int(input("Enter Size:"))
+arr=[]
+for i in range(size):
+    arr.append(input())
+print("Enter an element to search")
+element=[]
+chk=0
+for i in range(size):
+    if element==arr[i]:
+        index=i
+        chk=1
+if chk==1:
+    print("Element found at index",str(Index))
+else:
+    print("Not found")
