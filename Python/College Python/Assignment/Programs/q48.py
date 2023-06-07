0c7dc2884e43e331435ff379aa4598d5b0fad27b
prabhjyot009 <prabh156dps@gmail.com>
2023-06-07 21:35:01 +0530

    stuff

--- Python/College Python/Assignment/Programs/q48.py
+++ Python/College Python/Assignment/Programs/q48.py
@@ -12,10 +12,10 @@ print(df)
 #algorithm to Convert list of nested dictionary into Pandas dataframe
 #step1: start
 #step2: import pandas as pd
-#step4: define a variable Name and assign ['A','B','C','D','E'] to it
-#step5: define a variable Age and assign [20,21,22,23,24] to it
-#step6: define a variable dict1 and assign {'Name':Name,'Age':Age} to it
-#step7: print dict1
-#step8: define a variable df and assign pd.DataFrame(dict1) to it
-#step9: print df
-#step10: stop
+#step3: define a variable Name and assign ['A','B','C','D','E'] to it
+#step4: define a variable Age and assign [20,21,22,23,24] to it
+#step5: define a variable dict1 and assign {'Name':Name,'Age':Age} to it
+#step6: print dict1
+#step7: define a variable df and assign pd.DataFrame(dict1) to it
+#step8: print df
+#step9: stop
