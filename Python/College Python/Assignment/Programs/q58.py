9d7dbd7cc94fed5a76fb4bd90e247677e5f52340
prabhjyot009 <prabh156dps@gmail.com>
2023-06-19 11:30:24 +0530

    Stufff

--- Python/College Python/Assignment/Programs/q58.py
+++ Python/College Python/Assignment/Programs/q58.py
@@ -1,11 +1,11 @@
-# def valueerror(prompt):
-#     try:
-#         value=int(input())
-#         return value
-#     except ValueError:
-#         print("Error invalid input")
-# n=valueerror("Enter an integer")
-# print(n)
+def valueerror(prompt):
+    try:
+        value=int(input())
+        return value
+    except ValueError:
+        print("Error invalid input")
+n=valueerror("Enter an integer")
+print(n)
 
 x=5
 y="hello"
